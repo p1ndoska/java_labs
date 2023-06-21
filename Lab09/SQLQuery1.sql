1a9d90ecd3ac84ac87f29c3f68f9e886e8e17a3c
Anna <92574522+p1ndoska@users.noreply.github.com>
2023-06-21 15:56:16 +0300

    Add files via upload

---
+++ Lab09/SQLQuery1.sql
@@ -0,0 +1,9 @@
+﻿create table Users
+(
+	ID int identity(1, 1) PRIMARY KEY,
+	User_Login nvarchar(50) NOT NULL,
+	User_Password nvarchar(50) NOT NULL,
+	User_Role nvarchar(10) NOT NULL
+)
+insert into Users ([User_Login], [User_Password], [User_Role]) VALUES (N'admin', N'⌡⤯坺ꞥ襃๊聊쌟', N'admin')
+insert into Users ([User_Login], [User_Password], [User_Role]) VALUES (N'user1', N'줤廡꽒糄嬢繵鴟', N'user')
